49fa175d87d73be80eb6443bab0bdc1b8480b2e8
y80x86ol <364810520@qq.com>
2016-09-29 11:38:03 +0800

    完善域名模块

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # laravel-modules
 modules for laravel5
+
+## How To Install
+
+You can add in composer.json for laravel5 like this:
+
+<pre><code>
+"require": {
+        "y80x86ol/laravel-modules": "0.1.x-dev"
+    },
+</code></pre>
+
+and run 
+    composer update
+
+## How To Use
--- test/DomainTest/Http/Controllers/DomainTestController.php
+++ test/DomainTest/Http/Controllers/DomainTestController.php
@@ -1,19 +1,27 @@
 <?php
 
+/**
+ * sub domain test controller
+ */
 
 namespace App\Modules\Domain\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
-class DomainController extends Controller
-{
+class DomainController extends Controller {
+
+    /**
+     * home page
+     */
     public function index() {
-        echo "hello domain home page";
-        die("over");
+        echo "hello sub domain home page";
     }
 
+    /**
+     * hello
+     */
     public function hello() {
-        echo "hello domain1";
+        echo "hello sub domain";
     }
 
 }
